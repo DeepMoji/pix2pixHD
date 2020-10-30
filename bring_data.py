eafbba7c03d9de6e6d4695a2dda1688703c9d8d3
michael kolomenkin <michael.kolomenkin@gmail.com>
2020-10-30 19:31:03 +0200

    Script that brings aligned images for all toons

---
+++ bring_data.py
@@ -0,0 +1,26 @@
+import os
+from os import listdir
+from os.path import isfile, join
+
+
+if __name__ == "__main__":
+    print('Bring training data')
+
+    # Go over the toons file and bring from the cloud the missing raw files
+    toons_dir = '/home/pix2pixHD/datasets/toonify/toons' # should be input later
+    raw_dir = '/home/pix2pixHD/datasets/toonify/raw' # should be input later
+    aligned_dir = '/home/pix2pixHD/datasets/toonify/aligned'  # should be input later
+
+    toon_files = [f for f in listdir(toons_dir) if isfile(join(toons_dir, f))]
+    for toon in toon_files:
+        # Check if the corresponding file exists in aligned folder
+        # remove the '-toon' from the filename
+        if 'toon' not in toon:
+            continue
+        aligned_name = toon[0:8] + '.png'
+        if isfile(join(aligned_dir, aligned_name)):
+            continue
+        # If not bring it
+        os.system('gsutil cp gs://deepmoji.appspot.com/mk_results/aligned_ffhq/' + aligned_name + ' ' +
+                  join(aligned_dir, aligned_name))
+        pass
