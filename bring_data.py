218b8051c358c012acf9bcbe27355af67782475a
michael kolomenkin <michael.kolomenkin@gmail.com>
2020-10-30 19:39:49 +0200

    added logging of file name to bring_data.py

--- bring_data.py
+++ bring_data.py
@@ -1,6 +1,7 @@
 import os
 from os import listdir
 from os.path import isfile, join
+import logging
 
 
 if __name__ == "__main__":
@@ -10,6 +11,9 @@ if __name__ == "__main__":
     toons_dir = '/home/pix2pixHD/datasets/toonify/toons' # should be input later
     raw_dir = '/home/pix2pixHD/datasets/toonify/raw' # should be input later
     aligned_dir = '/home/pix2pixHD/datasets/toonify/aligned'  # should be input later
+
+    logging.basicConfig(level=logging.INFO, filename='/home/pix2pixHD/bring.log')
+    logging.info('start bringing')
 
     toon_files = [f for f in listdir(toons_dir) if isfile(join(toons_dir, f))]
     for toon in toon_files:
@@ -23,4 +27,5 @@ if __name__ == "__main__":
         # If not bring it
         os.system('gsutil cp gs://deepmoji.appspot.com/mk_results/aligned_ffhq/' + aligned_name + ' ' +
                   join(aligned_dir, aligned_name))
+        logging.info('Brought ' + aligned_name)
         pass
