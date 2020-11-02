32e72a557a3355fd9c044cca5a93df887da1c29e
michael kolomenkin <michael.kolomenkin@gmail.com>
2020-11-02 18:47:11 +0200

    fractions.gcd replaced with math.gcd

--- train.py
+++ train.py
@@ -4,9 +4,10 @@ import numpy as np
 import torch
 from torch.autograd import Variable
 from collections import OrderedDict
+import math
 from subprocess import call
 import fractions
-def lcm(a,b): return abs(a * b)/fractions.gcd(a,b) if a and b else 0
+def lcm(a,b): return abs(a * b)/math.gcd(a,b) if a and b else 0
 
 from options.train_options import TrainOptions
 from data.data_loader import CreateDataLoader
