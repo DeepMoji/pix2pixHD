514cf3585a1fa6ad752df2a3ff5999d7f4ebce9d
michael kolomenkin <michael.kolomenkin@gmail.com>
2020-12-03 02:00:39 +0200

    Remove gradient from computation

--- coreml_mac_convert.py
+++ coreml_mac_convert.py
@@ -9,6 +9,10 @@ from os.path import isfile, join
 from PIL import Image
 import numpy as np
 import cv2 as cv
+from torch import nn
+import torch.nn.functional as F
+
+import torch
 
 
 def convert_32_to_16_1():
@@ -83,6 +87,7 @@ def single_test():
     img_name = '/Users/michaelko/Code/pix2pixHD/results/linux_160/images/12400_01_input_label.jpg'
     model_name = '/Users/michaelko/Code/pix2pixHD/checkpoints/label2city/latest_net_G.mlmodel'
     model = ct.models.MLModel(model_name)
+
     img = Image.open(img_name)
     img = img.resize((512, 512))
 
@@ -98,6 +103,41 @@ def single_test():
     cv.imwrite('/Users/michaelko/Downloads/example_011.png', 255*rgb)
 
 
+class LeNet(nn.Module):
+    def __init__(self):
+        super(LeNet, self).__init__()
+        # 1 input image channel, 6 output channels, 3x3 square conv kernel
+        self.conv1 = nn.Conv2d(1, 6, 3)
+        self.conv2 = nn.Conv2d(6, 16, 3)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)  # 5x5 image dimension
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+        torch.nn.init.xavier_uniform_(self.conv1.weight)
+        torch.nn.init.xavier_uniform_(self.conv2.weight)
+        torch.nn.init.xavier_uniform_(self.fc1.weight)
+        torch.nn.init.xavier_uniform_(self.fc2.weight)
+        torch.nn.init.xavier_uniform_(self.fc3.weight)
+
+    def forward(self, x):
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = x.view(-1, int(x.nelement() / x.shape[0]))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+def pruning_example():
+    pass
+    device = torch.device("cpu")
+
+    model = LeNet().to(device=device)
+    module = model.conv1
+    print(list(module.named_parameters()))
+
+    print(model)
+
 if __name__ == '__main__':
     print('Convert models')
     print('Usage for pytorch to mlmodel:')
@@ -110,7 +150,21 @@ if __name__ == '__main__':
     # Get input parameters
     # convert_32_to_16_1()
     # compare_models()
-    single_test()
+    # single_test()
+
+    # pruning_example()
+
+
+    in_folder = "/Users/michaelko/Code/pix2pixHD/datasets/toonify/test"
+    out_folder = "/Users/michaelko/Code/pix2pixHD/datasets/toonify/test_jpg"
+
+    onlyfiles = [f for f in listdir(in_folder) if isfile(join(in_folder, f))]
+
+    for m_f in onlyfiles:
+        img = cv.imread(join(in_folder, m_f))
+        out_name = join(out_folder, m_f)
+        cv.imwrite(out_name[:-3] + "jpg", img);
+
 
 
     pass
